396402c3863ec6862ca91551b2862e9b56d4fb5e
qinhuaigan <624518812@qq.com>
2021-01-18 14:24:24 +0800

    更新 demo

--- server/boot/role-resolver.js
+++ server/boot/role-resolver.js
@@ -1,14 +1,177 @@
+const log4js = require('log4js');
+log4js.configure(require('../../config/log4js'))
+const log = log4js.getLogger('Role');
+const requireData = require('../../json/requireData')
+const formidable = require("formidable");
+const {
+  postData,
+  formatFormData
+} = require('../service/service.js')
+
+async function checkRequireData(context) { // 检验必填字段
+  const checkData = requireData[context.modelName] ? requireData[context.modelName][context.method] : null
+  let formData = null
+  if (checkData) {
+    const source = checkData.source // 参数存放位置
+    const form = new formidable.IncomingForm(); // 处理 form 表单数据
+    formData = {}
+    if (context.remotingContext.req.headers['content-type'].indexOf('multipart/form-data') >= 0) {
+      formData = await new Promise((resolve) => { // formdata 中的参数
+        form.parse(context.remotingContext.req, (err, fields, files) => {
+          resolve({
+            fields,
+            files
+          })
+        });
+      })
+    }
+    // 存在需要 "必填字段" 约束，所以，需要验证
+    const keys = Object.keys(checkData)
+    const bodyData = context.remotingContext.req.body // body 中的参数
+    const queryData = context.remotingContext.req.query // query 中的参数
+    let data // 请求数据
+    switch (source) {
+      case 'formdata':
+        data = formData.fields
+        break;
+      case 'query':
+        data = queryData
+        break;
+      case 'body':
+        data = bodyData
+        break;
+      default:
+        data = bodyData
+        break;
+    }
+    let msg = ''
+    for (const key in checkData.data) {
+      if (checkData.data[key].required && !data[key]) {
+        msg += `${checkData.data[key].description}、`
+      }
+    }
+    if (msg) {
+      return {
+        result: false,
+        msg,
+        data,
+        source,
+        file: formData.files.file
+      }
+    } else {
+      return {
+        result: true,
+        msg,
+        data,
+        source,
+        file: formData.files.file
+      }
+    }
+  }
+}
+
 module.exports = function(app) {
   const Role = app.models.Role;
   const AccessToken = app.models.AccessToken;
   const User = app.models.User;
   Role.registerResolver('$owner', function(role, context, cb) {
-    const error = new Error()
-    error.message = '需要授权'
-    error.statusCode = 401
-    // 当前账号是否属于 "$owner" 角色
-    const token = context.remotingContext.req.query.token
-    if (!token) return error
-    return cb(null, true);
+    // 系统管理员
+    (async function() {
+      const error = new Error()
+      error.message = '需要授权'
+      error.statusCode = 401
+      // 验证权限之前，先验证必填字段是否完整
+      const checkResult = await checkRequireData(context)
+      if (checkResult.msg) {
+        error.message = `${checkResult.msg.substr(0, checkResult.msg.length - 1)}不能为空`
+        log.error(error)
+        return cb(error, false)
+      }
+
+      // 当前账号是否属于 "$owner" 角色
+      const token = context.remotingContext.req.query.access_token
+      if (!token) {
+        return cb(error, false)
+      }
+      const resolved = await postData('AccessToken', 'resolve', token)
+      if (!resolved || !resolved.userId) {
+        return cb(error, false)
+      }
+
+      const user = await postData('User', 'findById', resolved.userId)
+      if (user && user.identity === 1) {
+        // 身份符合（管理员身份）
+        if (checkResult.source === 'formdata') {
+          context.remotingContext.req.body = Object.assign({
+            file: checkResult.file
+          }, checkResult.data)
+        }
+        return cb(null, true);
+      } else {
+        return cb(error, false)
+      }
+    })()
+  });
+
+  Role.registerResolver('$general', function(role, context, cb) {
+    // 所有用户（已注册过的用户）
+    (async function() {
+      const error = new Error()
+      error.message = '需要授权'
+      error.statusCode = 401
+      // 验证权限之前，先验证必填字段是否完整
+      const checkResult = await checkRequireData(context)
+      if (checkResult && checkResult.msg) {
+        error.message = `${checkResult.msg.substr(0, checkResult.msg.length - 1)}不能为空`
+        log.error(error)
+        return cb(error, false)
+      }
+
+      // 当前账号是否属于 "$general" 角色（注册过的账号）
+      const token = context.remotingContext.req.query.access_token
+      if (!token) {
+        return cb(error, false)
+      }
+
+      const resolved = await postData('AccessToken', 'resolve', token)
+      if (!resolved || !resolved.userId) {
+        return cb(error, false)
+      }
+
+      const user = await postData('User', 'findById', resolved.userId)
+      if (user && user.identity >= 1) {
+        // 身份符合（管理员身份）
+        if (checkResult && checkResult.source === 'formdata') {
+          context.remotingContext.req.body = Object.assign({
+            file: checkResult.file
+          }, checkResult.data)
+        }
+        return cb(null, true);
+      } else {
+        return cb(error, false)
+      }
+    })()
+  });
+
+  Role.registerResolver('$everyone', function(role, context, cb) {
+    // 所有用户（包括非游客）
+    (async function() {
+      const error = new Error()
+      error.message = '需要授权'
+      error.statusCode = 401
+      // 验证权限之前，先验证必填字段是否完整
+      const checkResult = await checkRequireData(context)
+      if (checkResult && checkResult.msg) {
+        error.message = `${checkResult.msg.substr(0, checkResult.msg.length - 1)}不能为空`
+        log.error(error)
+        return cb(error, false)
+      }
+      if (checkResult && checkResult.source === 'formdata') {
+        context.remotingContext.req.body = Object.assign({
+          file: checkResult.file
+        }, checkResult.data)
+      }
+      return cb(null, true)
+    })()
   });
 };
--- server/service/service.js
+++ server/service/service.js
@@ -1,7 +1,11 @@
 const app = require('../server.js');
 const log4js = require('log4js');
-log4js.configure(require('../../config/log4js'))
+const FormData = require('form-data');
+log4js.configure(require('../../config/log4js'));
 const log = log4js.getLogger('service.js');
+const fs = require('fs');
+const path = require('path');
+// 数据库操作函数
 function postData(model, fun, parm1, parm2) {
   return new Promise((resolve) => {
     if (parm1 && parm2) {
@@ -9,45 +13,159 @@ function postData(model, fun, parm1, parm2) {
         app.models[model][fun](parm1, parm2, (err, response) => {
           try {
             if (err) {
-              log.error(err)
-              resolve(false)
+              console.log(err);
+              log.error(err);
+              resolve(false);
             } else {
-              resolve(response)
+              resolve(response);
             }
           } catch (e) {
-            log.error(e)
-            //TODO handle the exception
+            console.log(e);
+            log.error(e);
+            // TODO handle the exception
           }
-        })
+        });
       } catch (e) {
-        log.error(e)
-        resolve(false)
-        //TODO handle the exception
+        console.log(e);
+        log.error(e);
+        resolve(false);
+        // TODO handle the exception
       }
     } else {
       try {
         app.models[model][fun](parm1, (err, response) => {
           try {
-            if(err) {
-              log.error(err)
-              resolve(false)
+            if (err) {
+              console.log(err);
+              log.error(err);
+              resolve(false);
             } else {
-              resolve(response)
+              resolve(response);
             }
           } catch (e) {
-            log.error(e)
-            //TODO handle the exception
+            console.log(e);
+            log.error(e);
+            // TODO handle the exception
           }
-        })
+        });
       } catch (e) {
-        log.error(e)
-        //TODO handle the exception
-        resolve(false)
+        log.error(e);
+        // TODO handle the exception
+        resolve(false);
       }
     }
-  })
+  });
 }
 
-module.exports = {
-  postData
+// 获取文件
+function getFileByContainer(container) {
+  return new Promise((resolve) => {
+    app.models.Container.getFiles(`${container}`, (err, response) => {
+      try {
+        if (err) {
+          log.error(err);
+          log.error('log error in line:59');
+          return resolve(false);
+        }
+        const result = [];
+        for (let i = 0; i < response.length; i++) {
+          result.push({
+            filename: response[i].filename,
+            path: `/Containers/${container}/download/${response[i].filename}`,
+            container: `${container}`,
+          });
+        }
+        resolve(result);
+      } catch (e) {
+        log.error(e);
+        log.error('log catch in line:65');
+        // TODO handle the exception
+        resolve(false);
+      }
+    });
+  });
 }
+
+// 日期格式化
+function formatDate(value, type) {
+  if (!value) {
+    return null;
+  }
+  const date = new Date(value);
+  let fmt = type || 'yyyy-MM-dd';
+  var obj = {
+    'y': date.getFullYear(), // 年份，注意必须用getFullYear
+    'M': date.getMonth() + 1, // 月份，注意是从0-11
+    'd': date.getDate(), // 日期
+    'q': Math.floor((date.getMonth() + 3) / 3), // 季度
+    'w': date.getDay(), // 星期，注意是0-6
+    'H': date.getHours(), // 24小时制
+    'h': date.getHours() % 12 === 0 ? 12 : date.getHours() % 12, // 12小时制
+    'm': date.getMinutes(), // 分钟
+    's': date.getSeconds(), // 秒
+    'S': date.getMilliseconds(), // 毫秒
+  };
+  var week = ['天', '一', '二', '三', '四', '五', '六'];
+  for (var i in obj) {
+    fmt = fmt.replace(new RegExp(i + '+', 'g'), function(m) {
+      var val = obj[i] + '';
+      if (i === 'w') return (m.length > 2 ? '星期' : '周') + week[val];
+      for (var j = 0, len = val.length; j < m.length - len; j++) {
+        val = '0' + val;
+      }
+      return m.length === 1 ? val : val.substring(val.length - m.length);
+    });
+  }
+  return fmt;
+}
+
+// 创建文件流
+function createFileStream(file) {
+  return new Promise((resolve) => {
+    const oldPath = file.path; // 这里的路径是图片的本地路径
+    const newPath = path.join(path.dirname(oldPath), file.name); // 这里的路径是图片的新的本地路径
+    // 重命名文件
+    fs.rename(oldPath, newPath, (err) => {
+      resolve(fs.createReadStream(newPath)); // 'file'是服务器接受的key，创建文件流
+    });
+  });
+}
+
+// 将 json 转化为 formdata
+function formatFormData(data) {
+  const form = new FormData();
+  const promises = [];
+  const keys = [];
+  return new Promise((resolve) => {
+    if (data) {
+      for (let key in data) {
+        if (data[key] !== undefined && data[key] !== null) {
+          console.log('key ===', key);
+          keys.push(key);
+          if (key === 'file') {
+            promises.push(createFileStream(data[key]));
+          } else {
+            promises.push(new Promise((resolve2) => {
+              resolve2(data[key]);
+            }));
+          }
+        }
+      }
+      Promise.all(promises).then((result) => {
+        console.log('keys ===', keys);
+        console.log('转换后', result);
+        for (let i = 0; i < keys.length; i++) {
+          form.append(keys[i], result[i]);
+        }
+        console.log('form ====', form);
+        resolve(form);
+      });
+    }
+  });
+}
+module.exports = {
+  postData,
+  getFileByContainer,
+  formatDate,
+  formatFormData,
+};
